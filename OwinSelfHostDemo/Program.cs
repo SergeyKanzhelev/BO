15eadcdc5f2f195ac8be673bb7d4f8acadf53b8f
Alex Bulankou <alexb@stopbystop.com>
2016-01-22 13:33:04 -0800

    Fixing build on 4.6

--- OwinSelfHostDemo/Program.cs
+++ OwinSelfHostDemo/Program.cs
@@ -17,6 +17,7 @@ namespace OwinSelfHostDemo
             // Start OWIN host 
             using (WebApp.Start<Startup>(url: baseAddress))
             {
+                Console.ReadLine();
                 // Create HttpCient and make a request to api/values 
                 HttpClient client = new HttpClient();
 
