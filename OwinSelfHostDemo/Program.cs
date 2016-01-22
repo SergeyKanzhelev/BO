5941067e2e9ade54b374f6fc08e101652eecff65
Sergey Kanzhelev <SergKanz@microsoft.com>
2016-01-21 20:57:59 -0800

    add delay and dependencies into self-host app

--- OwinSelfHostDemo/Program.cs
+++ OwinSelfHostDemo/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Owin.Hosting;
 using System.Net.Http;
 using Microsoft.ApplicationInsights.Extensibility;
+using System.Threading;
 
 namespace OwinSelfHostDemo
 {
@@ -19,13 +20,18 @@ namespace OwinSelfHostDemo
                 // Create HttpCient and make a request to api/values 
                 HttpClient client = new HttpClient();
 
+                Random rand = new Random();
+
                 for (int i = 0; i < 100; i++)
                 {
                     var response = client.GetAsync(baseAddress + "api/values").Result;
 
                     Console.WriteLine(response.Content.ReadAsStringAsync().Result);
 
-                    response = client.GetAsync(baseAddress + "api/values/" + i).Result;
+                    var idx = Convert.ToInt32(Math.Round(rand.NextDouble() * 100));
+                    Thread.Sleep(TimeSpan.FromMilliseconds(idx));
+                    
+                    response = client.GetAsync(baseAddress + "api/values/" + idx ).Result;
 
                     Console.WriteLine(response.Content.ReadAsStringAsync().Result);
 
--- OwinSelfHostDemo/ValuesController.cs
+++ OwinSelfHostDemo/ValuesController.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.ApplicationInsights;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace OwinSelfHostDemo
 {
     public class ValuesController : ApiController
     {
+        private static Random rand = new Random();
+
         // GET api/values 
         public IEnumerable<string> Get()
         {
-            
+            var idx = Convert.ToInt32(Math.Round(rand.NextDouble() * 100));
+            Thread.Sleep(TimeSpan.FromMilliseconds(idx));
+
             var client = new TelemetryClient();
             client.TrackTrace("trace");
 
@@ -17,12 +25,14 @@ namespace OwinSelfHostDemo
         }
 
         // GET api/values/5 
-        public string Get(int id)
+        public async Task<string> Get(int id)
         {
             var client = new TelemetryClient();
             client.TrackTrace("trace");
 
-            return "value";
+            HttpClient httpClient = new HttpClient();
+
+            return await httpClient.GetStringAsync("http://bing.com");
         }
 
         // POST api/values 
