a3ed489e52fa17ed3dcff006b6cede9cefcf2a24
Sergey Kanzhelev <SergKanz@microsoft.com>
2016-02-01 12:00:27 -0800

    add one more instrumentation key

--- OwinSelfHostDemo/Startup.cs
+++ OwinSelfHostDemo/Startup.cs
@@ -16,8 +16,8 @@ namespace OwinSelfHostDemo
         {
             var configuration = TelemetryConfiguration.CreateDefault();
             // alex's ikey: "a7010d58-fd1a-4d1f-b385-4d041d7c558c";
-            configuration.InstrumentationKey = "c92059c3-9428-43e7-9b85-a96fb7c9488f";
-
+            configuration.InstrumentationKey = "c92059c3-9428-43e7-9b85-a96fb7c9488f"; //BO
+            //configuration.InstrumentationKey = "9d3ebb4f-7a11-4fb1-91ac-7ca8a17627eb"; //BO2
 
             // Configure Web API for self-host. 
             HttpConfiguration config = new HttpConfiguration();
